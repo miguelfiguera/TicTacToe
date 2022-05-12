f4762cfe841c9307a514f1bb35580217f03db655
Miguel Figuera <miguelqui725@gmail.com>
2022-05-12 17:15:58 -0400

    suffering

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,8 @@
-require_relative "display.rb"
+require_relative "text_module.rb"
 
 class Game
-    include "Display"
+    include "Text"
+    attr_reader :player_one, :player_two, :board, :current_player
     def initialize
         @board = Board.new
         @player_one = nil
@@ -17,8 +18,30 @@ class Game
         player=Player.new(name,symbol) 
     end
 
-    def game_setup 
-        creating_players(1,player_1)
-        creating_players(2,player_2) 
+    def game_prep
+        @player_one=creating_players(1,player_1)
+        @player_two=creating_players(2,player_2)
+    end
+
+    def full_board?
+        @positions.all? { |positions| positions =~ /[^0-9]/ }            
+        draw
+    end
+
+    def valid_move(number)
+        @positions[number]==number
+    end
+
+    def play_turns
+        @current_player = @player_one
+    end
+
+    def switch_current_player
+        if @current_player== @player_one
+            player_two
+        else
+            player_one
+        end
+    end
 
 end
